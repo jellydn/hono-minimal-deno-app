e89131d81367f9f9a4bc09e6adebddf97d599302
Huynh Duc Dung <dunghd.it@gmail.com>
2022-12-18 01:39:25 +0800

    :tada: Init project

---
+++ main.ts
@@ -0,0 +1,27 @@
+import { serve } from "http/server.ts";
+import { Hono } from "hono/mod.ts";
+import { cors, etag, logger } from "hono/middleware.ts";
+
+export const app = new Hono();
+
+// Builtin middleware
+app.use("*", etag(), logger());
+
+// Routing
+app.get("/", (c) => c.html("<h1>Hello Hono!</h1>"));
+app.notFound((c) => c.json({ message: "Not Found", ok: false }, 404));
+
+// Nested route
+const api = new Hono();
+api.use("/thread/*", cors());
+// Named path parameters
+api.get("/thread/:username/:id", (c) => {
+  const username = c.req.param("id");
+  const id = c.req.param("id");
+
+  return c.json({ Url: `https://twitter.com/${username}/status/${id}` });
+});
+
+app.route("/api", api);
+
+serve(app.fetch);
